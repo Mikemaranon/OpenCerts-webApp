a46c4da1068960fae28731be2e67ef93c5b37b75
Mikemaranon <mikemaranonx@gmail.com>
2024-11-21 09:13:17 +0100

    tell me why

--- chat.js
+++ chat.js
@@ -22,13 +22,31 @@ function sendMessage() {
     }
 }
 
+let tellMeWhy = 0;
+
 // Función para simular la respuesta de la IA
 function getAIResponse(userMessage) {
     // Simulamos una espera de 1 segundo para la respuesta
     setTimeout(() => {
         const aiMessageDiv = document.createElement('div');
         aiMessageDiv.classList.add('message', 'ai');
-        aiMessageDiv.textContent = "IA: Esta es una respuesta simulada a tu mensaje: " + userMessage;
+
+        switch(userMessage) {
+            case "Tell me why": 
+                    switch(tellMeWhy) {
+                        case 0: aiMessageDiv.textContent = "Ain't nothin' but a heartache";
+                            tellMeWhy++;
+                            break;
+                        case 1: aiMessageDiv.textContent = "Ain't nothin' but a mistake";
+                            tellMeWhy++;
+                            break;
+                        case 2: aiMessageDiv.textContent = "I never wanna hear you say... I want it that way";
+                            tellMeWhy = 0;
+                    }
+                break;
+            default: aiMessageDiv.textContent = "IA: Esta es una respuesta simulada a tu mensaje: " + userMessage;
+                tellMeWhy = 0;
+        }
         chatHistory.appendChild(aiMessageDiv);
 
         // Desplazar el historial hacia abajo
